63f4d386481e0afbc5786584441eb00a8520b340
Sritha-shanmugam <sritha.shanmugam@gmail.com>
2024-08-07 22:33:06 +0530

    Add files via upload

--- loginPageObjects.java
+++ loginPageObjects.java
@@ -3,7 +3,6 @@ package pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -14,10 +13,12 @@ public class loginPageObjects {
     By username=By.id("user-name");
     By password=By.id("password");
     By loginButton =By.id("login-button");
-//    By errorMessage =By.xpath("//h3[@data-test='error']");
+    By successIndicator=By.id("page_wrapper");
+    By errorMessage =By.xpath("//h3[@data-test='error']");
 
 
     public loginPageObjects(WebDriver driver){
+        //interact with browser
         this.driver=driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
@@ -30,5 +31,11 @@ public class loginPageObjects {
     }
     public void loginButton(){
         driver.findElement(loginButton).click();
+    }
+    public WebElement verifySuccessfulLogin(){
+        return driver.findElement(successIndicator);
+    }
+    public WebElement getErrorMessage(){
+        return driver.findElement(errorMessage);
     }
 }
--- loginSteps.java
+++ loginSteps.java
@@ -4,7 +4,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import pageObjects.loginPageObjects;
 
 public class loginSteps {
@@ -12,12 +14,13 @@ public class loginSteps {
     loginPageObjects loginPage;
 
     @Given("I open the login page")
-    public void i_open_the_login_page() {
+    public void i_open_the_login_page() throws InterruptedException {
         // Write code here that turns the phrase above into concrete actions
         System.getProperty("webdriver.chrome.driver", "C:\\Users\\ADMIN\\Downloads\\chromedriver-win64\\chromedriver.exe");
         driver=new ChromeDriver();
-        driver.get("https://www.saucedemo.com/v1/");
+        driver.get("https://www.saucedemo.com/v1/index.html");
         loginPage=new loginPageObjects(driver);
+        Thread.sleep(3000);
 
     }
     @When("I enter correct username and password")
@@ -31,6 +34,22 @@ public class loginSteps {
     @Then("I should be logged in successfully")
     public void i_should_be_logged_in_successfully() {
         // Write code here that turns the phrase above into concrete actions
+        WebElement successMessage=loginPage.verifySuccessfulLogin();
+        Assert.assertTrue(successMessage.isDisplayed(), "Login Successful");
+        driver.quit();
+    }
 
+    @When("I enter incorrect username and password")
+    public void i_Enter_Incorrect_Username_And_Password() {
+        loginPage.userName("sritha_user");
+        loginPage.password("secret_user");
+        loginPage.loginButton();
+    }
+
+    @Then("I should see an error message")
+    public void iShouldSeeAnErrorMessage() {
+        WebElement errorElement= loginPage.getErrorMessage();
+        Assert.assertTrue(errorElement.isDisplayed(), "Error not displayed");
+        driver.quit();
     }
 }
